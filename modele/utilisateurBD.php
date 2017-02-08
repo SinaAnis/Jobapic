344578c55d0afbdab62264ddc29e3eaf5b5cfcaa
CampusTrip <anis.akrouche21@gmail.com>
2017-02-08 20:29:42 +0100

    yo

--- modele/utilisateurBD.php
+++ modele/utilisateurBD.php
@@ -1,55 +1,76 @@
-<?php
-/*Fonctions-modèle réalisant les requètes de gestion des utilisateurs en base de données*/
+	<?php
+	/*Fonctions-modèle réalisant les requètes de gestion des utilisateurs en base de données*/
 
-// verif_ident : fonction booléenne de vérification de l'utilisateur en base de données
-// Si ok, alors le profil de l'utilisateur, à savoir son enregistrement, est affecté en sortie à $profil
+	// verif_ident : fonction booléenne de vérification de l'utilisateur en base de données
+	// Si ok, alors le profil de l'utilisateur, à savoir son enregistrement, est affecté en sortie à $profil
 
-function verif_ident($log='',$num='', &$profil) {
-	require ("./modele/connectBD.php");
-	$req= "SELECT * FROM Utilisateur WHERE idUser='%s' AND Mdp='%s'";
-	$sql = sprintf ($req, $log, $num);
-	$res = mysqli_query($link,$sql) or die ('erreur de requete : ' . $sql);
+	function verif_ident($log='',$num='', &$profil) {
+		require ("./modele/connectBD.php");
+		$req= "SELECT * FROM Utilisateur WHERE idUser='%s' AND Mdp='%s'";
+		$sql = sprintf ($req, $log, $num);
+		$res = mysqli_query($link,$sql) or die ('erreur de requete : ' . $sql);
 
-	if (mysqli_num_rows($res)>0) {
-		$profil=mysqli_fetch_assoc($res);
-		$_SESSION['profil']= $profil;	//Inclure dans une variable session le profil de l'user
-		return true;
+		if (mysqli_num_rows($res)>0) {
+			$profil=mysqli_fetch_assoc($res);
+			$_SESSION['profil']= $profil;	//Inclure dans une variable session le profil de l'user
+			return true;
+		}
+		else return false;
 	}
-	else return false;
-}
 
-function mail_disponible($mail=''){
-	require ("./modele/connectBD.php");
-	$req= "SELECT * FROM Utilisateur WHERE Mail ='%s'";
-	$sql = sprintf ($req, $mail);
-	$res = mysqli_query($link,$sql) or die ('erreur de requete : ' . $sql);
+	function mail_disponible($mail=''){
+		require ("./modele/connectBD.php");
+		$req= "SELECT * FROM Utilisateur WHERE Mail ='%s'";
+		$sql = sprintf ($req, $mail);
+		$res = mysqli_query($link,$sql) or die ('erreur de requete : ' . $sql);
 
-	if (mysqli_num_rows($res)>0) {
-		$profil=mysqli_fetch_assoc($res);
-		$_SESSION['profil']= $profil;	//Inclure dans une variable session le profil de l'user
-		return false;
+		if (mysqli_num_rows($res)>0) {
+			$profil=mysqli_fetch_assoc($res);
+			$_SESSION['profil']= $profil;	//Inclure dans une variable session le profil de l'user
+			return false;
+		}
+		else return true;
 	}
-	else return true;
-}
 
-function inscription($mail,$prenom,$nom,$mdp, $genre, $date, $ville , $telephone, $situation, &$profil) {
-	require ("./modele/connectBD.php");
-	$req= "INSERT INTO utilisateur (Nom,Prenom,Genre,DateNaissance,Adresse,Mail,Telephone,Situation,Mdp)
-	VALUES ('%s','%s','%s','%s','%s','%s','%s','%s','%s')";
-	$sql = sprintf ($req , $nom, $prenom, $genre ,$date , $ville, $mail, $telephone ,$situation , $mdp);
-	$res = mysqli_query($link,$sql) or die ('erreur de requete : ' . $sql);
+	function inscription($mail,$prenom,$nom,$mdp, $genre, $date, $ville , $telephone, $situation, &$profil) {
+		require ("./modele/connectBD.php");
+		$req= "INSERT INTO utilisateur (Nom,Prenom,Genre,DateNaissance,Adresse,Mail,Telephone,Situation,Mdp)
+		VALUES ('%s','%s','%s','%s','%s','%s','%s','%s','%s')";
+		$sql = sprintf ($req , $nom, $prenom, $genre ,$date , $ville, $mail, $telephone ,$situation , $mdp);
+		$res = mysqli_query($link,$sql) or die ('erreur de requete : ' . $sql);
 
-}
+	}
 
-function getProfil($log='',$num=''){
-	require ("./modele/connectBD.php");
-	$req= "SELECT nom,prenom FROM Utilisateur WHERE idUser='%s' AND Mdp='%s'";
-	$sql = sprintf ($req, $log, $num);
-	$res = mysqli_query($link,$sql) or die ('erreur de requete : ' . $sql);
-		foreach($res as $row)
-		  $_SESSION['nom'] = $row['nom'];
-			$_SESSION['prenom'] = $row['prenom'];
-}
+	function getProfil($log='',$num=''){
+		require ("./modele/connectBD.php");
+			$req= "SELECT idUser,nom,prenom,genre,dateNaissance,Adresse,Mail,Telephone,Situation,mdp FROM Utilisateur WHERE idUser='%s' AND Mdp='%s'";
+			$sql = sprintf ($req, $log, $num);
+			$res = mysqli_query($link,$sql) or die ('erreur de requete : ' . $sql);
+				foreach($res as $row){
+					$_SESSION['idUser'] = $row['idUser'];
+				  $_SESSION['nom'] = $row['nom'];
+					$_SESSION['prenom'] = $row['prenom'];
+					$_SESSION['genre'] = $row['genre'];
+				  $_SESSION['dateNaissance'] = $row['dateNaissance'];
+					$_SESSION['Adresse'] = $row['Adresse'];
+					$_SESSION['Mail'] = $row['Mail'];
+					$_SESSION['Telephone'] = $row['Telephone'];
+					$_SESSION['Situation'] = $row['Situation'];
+					$_SESSION['mdp'] = $row['mdp'];
+				}
+
+	}
+
+	function getCategorieFavoris($id){
+		require ("./modele/connectBD.php");
+			$req= "SELECT categorie.IdCategorie,categorie.NomCategorie
+FROM `categorie`,`categoriepref`
+WHERE idUser='%s'
+AND categorie.idCategorie = categoriepref.idCategorie";
+			$sql = sprintf ($req, $id);
+			$res = mysqli_query($link,$sql) or die ('erreur de requete : ' . $sql);
+			return $res;
+	}
 
 
 
