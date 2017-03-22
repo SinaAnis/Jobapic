42d6d03b4cd9423bb2989c70f24fab90efdbd1b9
Traumi <brsjdtendo@gmail.com>
2017-03-22 15:48:40 +0100

    Maj

--- controle/afficherAnnonce.php
+++ controle/afficherAnnonce.php
@@ -1,7 +1,12 @@
 <?php
 
 function afficheAnnonce($id){
-    
+    require('./modele/annonceBD.php');
+    $iduser=$_SESSION['idUser'];
+    $annonce= afficherAnnonceById($id);
+    $neme = getUserNameWithAnnonceId($id);
+    $tomcat = getCatNameWithAnnonceId($id);
+    require('./vue/uneAnnonce.tpl') ;
 }
 
 function create () {
