4b1850a1d8b894f2f8b00bf6fea594f61db360a2
Traumi <brsjdtendo@gmail.com>
2017-03-23 14:21:45 +0100

    Update mesCandidatures.php

--- controle/mesCandidatures.php
+++ controle/mesCandidatures.php
@@ -1,9 +1,10 @@
 <?php
 
-
 function mesCandidatures(){
+    require('./modele/annonceBD.php');
+	$iduser=$_SESSION['idUser'];
+    $tabAnnonces = getPostuleById($iduser);
 	require('./vue/mesCandidatures.tpl');
-	
 }
 
 ?>
